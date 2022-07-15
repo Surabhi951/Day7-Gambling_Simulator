d7d5a0948ce902fee80398b50915c87ec3a1f56b
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-15 17:41:17 +0530

    Resign for the day if won or lost

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,20 +1,22 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Gambler Simulator Problem");
         int stack = 100;
         int bet = 1;
         int totalAmount = 100;
 
-        double gameCheck = Math.floor(Math.random() * 10) % 2;
-        if (bet == gameCheck) {
-            System.out.println("you won the game");
-            totalAmount++;
-        } else {
-            System.out.println("you lost the game");
-            totalAmount--;
+        while (totalAmount > 50 && totalAmount < 150) {
+            double gameCheck = Math.floor(Math.random() * 10) % 2;
+            if (bet == gameCheck) {
+                System.out.println("you won the game");
+                totalAmount++;
+            } else {
+                System.out.println("you lost the game");
+                totalAmount--;
+            }
+            System.out.println("Total amount :" + totalAmount);
         }
-        System.out.println("Total amount :" + totalAmount);
     }
 }
