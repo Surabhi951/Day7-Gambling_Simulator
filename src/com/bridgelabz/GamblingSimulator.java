ba87194eff1ac40cef418ffc69dec1d4c7d3dbb5
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-15 17:13:02 +0530

    added message

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class GamblingSimulator {
+    public static void main(String[] args){
+        System.out.println("Welcome to Gambler Simulator Problem");
+        int stack = 100;
+        int bet = 1;
+    }
+}
