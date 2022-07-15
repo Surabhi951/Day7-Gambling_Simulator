4c0d08405984771ac68fffb3f0c12bf0cd3dd085
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-15 17:49:21 +0530

    After 20 days total amount won or lost

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,20 +3,38 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Simulator Problem");
-        int stack = 100;
+        int wins = 0;
+        int loss = 0;
         int bet = 1;
-        int totalAmount = 100;
 
-        while (totalAmount > 50 && totalAmount < 150) {
-            double gameCheck = Math.floor(Math.random() * 10) % 2;
-            if (bet == gameCheck) {
-                System.out.println("you won the game");
-                totalAmount++;
-            } else {
-                System.out.println("you lost the game");
-                totalAmount--;
+        for (int day = 1; day <= 20; day++) {
+            int stake = 100;
+            while (stake > 50 && stake < 150) {
+                double gameCheck = Math.floor(Math.random() * 10) % 2;
+                if (bet == gameCheck) {
+                    //System.out.println("you won the game");
+                    stake++;
+                } else {
+                    //System.out.println("you lost the game");
+                    stake--;
+                }
             }
-            System.out.println("Total amount :" + totalAmount);
+            if (stake == 50) {
+                loss++;
+                System.out.println("Day " + day + " Lost ");
+            } else {
+                wins++;
+                System.out.println("Day " + day + " Won ");
+            }
+        }
+
+        int finalAmount = wins * 50 - loss * 50;
+        if (finalAmount > 0)
+            System.out.println("After 20 days Gambler won: $ " + finalAmount);
+        else if (finalAmount < 0)
+            System.out.println("After 20 days Gambler lost: $ " + finalAmount);
+        else
+            System.out.println("No profit or loss ");
         }
     }
-}
+
